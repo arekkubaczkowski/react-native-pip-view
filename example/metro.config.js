6e566086fb93e5d0586327f079b1c9587495b27f
Arek Kubaczkowski <arek.kubaczkowski@gmail.com>
2025-04-18 08:29:07 +0200

    fix: example app

--- example/metro.config.js
+++ example/metro.config.js
@@ -2,8 +2,17 @@ const path = require('path');
 const { getDefaultConfig } = require('@expo/metro-config');
 const { getConfig } = require('react-native-builder-bob/metro-config');
 const pkg = require('../package.json');
+const {
+  wrapWithReanimatedMetroConfig,
+} = require('react-native-reanimated/metro-config');
 
 const root = path.resolve(__dirname, '..');
+
+const config = getConfig(getDefaultConfig(__dirname), {
+  root,
+  pkg,
+  project: __dirname,
+});
 
 /**
  * Metro configuration
@@ -11,8 +20,4 @@ const root = path.resolve(__dirname, '..');
  *
  * @type {import('metro-config').MetroConfig}
  */
-module.exports = getConfig(getDefaultConfig(__dirname), {
-  root,
-  pkg,
-  project: __dirname,
-});
+module.exports = wrapWithReanimatedMetroConfig(config);
